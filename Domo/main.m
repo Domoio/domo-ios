bf3483593f4077197f15ea264b5af0242a9ac387
Alexander List <alist@listprojects.com>
2013-06-25 13:07:41 -0400

    first of commit of all sorts of files

---
+++ Domo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Domo
+//
+//  Created by Alexander List on 6/25/13.
+//  Copyright (c) 2013 ExoMachina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DOAppDelegate class]));
+	}
+}
