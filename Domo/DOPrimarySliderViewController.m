f2e6bc5218a5cde85248200b1a9b8ea12fc735df
Alexander List <alist@listprojects.com>
2013-06-26 08:56:27 -0400

    added all the views

---
+++ Domo/DOPrimarySliderViewController.m
@@ -0,0 +1,38 @@
+//
+//  DOPrimarySliderViewController.m
+//  Domo
+//
+//  Created by Alexander List on 6/25/13.
+//  Copyright (c) 2013 ExoMachina. All rights reserved.
+//
+
+#import "DOPrimarySliderViewController.h"
+
+@interface DOPrimarySliderViewController ()
+
+@end
+
+@implementation DOPrimarySliderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:@"DOPrimarySliderViewController-iPhone" bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Domo/DORequestAdviceVC.m
@@ -0,0 +1,38 @@
+//
+//  DORequestAdviceVC.m
+//  Domo
+//
+//  Created by Alexander List on 6/25/13.
+//  Copyright (c) 2013 ExoMachina. All rights reserved.
+//
+
+#import "DORequestAdviceVC.h"
+
+@interface DORequestAdviceVC ()
+
+@end
+
+@implementation DORequestAdviceVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:@"DORequestAdviceVC-iPhone" bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
